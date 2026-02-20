feeff2bd448aad49b3909546fec232ee259f8dc3
Ammara-u <ammaraubaid10@gmail.com>
2026-02-20 01:11:40 -0800

    first

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -2,7 +2,9 @@ import os
 import dj_database_url
 from .settings import *
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME'), 'repo-backend-nlzr.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 # Fixed the 'G' to 'F' and added the missing 'get'
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')] 
 
@@ -34,10 +36,27 @@ STORAGES={
         "BACKEND":"whitenoise.storage.CompressedStaticFilesStorage"
     },
 }
-DATABASES={
-    'default':dj_database_url.config(
-default=os.environ['DATABASE_URL'],
-conn_max_age=600
-    )
+
+# In core/deployment_settings.py
+
+# Delete the dj_database_url block and replace it with this:
+DATABASES = {
+    'default': {       
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ammara_InventoryApp',
+        'HOST': '102.223.187.41',  # <--- REPLACE with your cPanel Shared IP
+        'PORT': '3306',
+        'USER': 'ammara_ammara',
+        'PASSWORD': 'ammara@1104',
+    }
 }
+# This block of code is configuring the database settings using the `dj_database_url` library in a
+# Django project. Here's what it's doing:
+
+# DATABASES={
+#     'default':dj_database_url.config(
+# default=os.environ['DATABASE_URL'],
+# conn_max_age=600
+#     )
+# }
 
